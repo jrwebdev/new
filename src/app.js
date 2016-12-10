719cb92d57ab3411ccda73af072bf68f2ace8853
jrwebdev <web@jravenscroft.co.uk>
2016-12-10 14:52:21 +1300

    Use system import for code splitting

--- src/app.js
+++ src/app.js
@@ -3,9 +3,38 @@ import ReactDOM from 'react-dom';
 
 import {BrowserRouter, Match, Miss, Link} from 'react-router';
 
-import A from './components/a';
-import B from './components/b';
-import C from './components/c';
+System.import('./util/hello').then(module => {
+  module.default();
+});
+
+// import A from './components/a';
+// import B from './components/b';
+// import C from './components/c';
+
+const asyncComponent = loadComponent => {
+
+  let Component;
+
+  return class AsyncComponent extends React.Component {
+
+    state = {
+      loaded: false
+    };
+
+    componentWillMount() {
+      loadComponent().then(LoadedComponent => {
+        Component = LoadedComponent;
+        this.setState({loaded: true});
+      });
+    }
+
+    render() {
+      return this.state.loaded ? <Component /> : <div>Loading...</div>;
+    }
+
+  };
+
+};
 
 const Nav = () => (
   <div style={{marginBottom: 20}}>
@@ -15,14 +44,19 @@ const Nav = () => (
 const Home = () => <div>Home</div>;
 const Err404 = () => <div>404 - Page Not Found</div>;
 
+const getDefault = module => module.default;
+const AsyncA = asyncComponent(() => System.import('./components/a').then(getDefault));
+const AsyncB = asyncComponent(() => System.import('./components/b').then(getDefault));
+const AsyncC = asyncComponent(() => System.import('./components/c').then(getDefault));
+
 const App = () => (
   <BrowserRouter>
     <div>
       <Nav />
       <Match exactly pattern="/" component={Home} />
-      <Match pattern="/a" component={A} />
-      <Match pattern="/b" component={B} />
-      <Match pattern="/c" component={C} />
+      <Match pattern="/a" component={AsyncA} />
+      <Match pattern="/b" component={AsyncB} />
+      <Match pattern="/c" component={AsyncC} />
       <Miss component={Err404} />
     </div>
   </BrowserRouter>
---
+++ src/util/hello.js
@@ -0,0 +1,5 @@
+console.log('Hello');
+
+export default () => {
+  console.log('Hello World!');
+};
